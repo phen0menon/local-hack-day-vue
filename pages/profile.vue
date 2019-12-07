e4e8a97daf0f13b5a64fafc22fd3fdf0f92bc816
gleb.potapov.0138@gmail.com <glebpotapov2001@gmail.com>
2019-12-07 12:29:49 +0300

    Make simple profile page design

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,3 +1,24 @@
 <template>
-  
+  <v-img
+    height="100%"
+    src="https://cdn.vuetifyjs.com/images/cards/server-room.jpg"
+  >
+    <v-row align="end" class="fill-height">
+      <v-col align-self="start" class="pa-0" cols="12">
+        <v-avatar class="profile" color="grey" size="164" tile>
+          <v-img
+            src="https://www.speridian.com/wp-content/uploads/2019/07/studentalign.jpg"
+          ></v-img>
+        </v-avatar>
+      </v-col>
+      <v-col class="py-0">
+        <v-list-item color="rgba(0, 0, 0, .4)" dark>
+          <v-list-item-content>
+            <v-list-item-title class="title">Good student</v-list-item-title>
+            <v-list-item-subtitle>Good student</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-col>
+    </v-row>
+  </v-img>
 </template>
